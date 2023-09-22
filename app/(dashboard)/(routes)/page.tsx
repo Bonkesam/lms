4f49780a99c61809a6083d71dec2ddb08ed0d356
Bonkesam <bonkesam9@gmail.com>
2023-09-22 23:39:53 +0300

    Sets up the environment and authentication using Clerk Auth

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton  
+        afterSignOutUrl='/'
+      />
+    </div>
+  )
+}
+
